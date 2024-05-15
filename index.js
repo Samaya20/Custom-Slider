5e3fad6534470254f3083ae88dfce5e3827ef90c
Samaya20 <mughumova@gmail.com>
2024-05-15 17:38:17 +0400

    v2

--- index.js
+++ index.js
@@ -1,70 +1,40 @@
 const slider = document.querySelector(".slider");
-const sliderContainer = document.querySelector(".slider-container");
-let isDown = false;
-let startX;
-let scrollLeft;
+const thumbs = document.querySelectorAll(".thumb-item");
+const thumbsContainer = document.querySelector(".thumbs");
 
-slider.addEventListener("mousedown", (e) => {
-  isDown = true;
-  slider.classList.add("active");
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
+thumbs.forEach((thumb) => {
+  thumb.addEventListener("click", function () {
+    const thumbSrc = this.getAttribute("src");
+    const matchedSlide = slider.querySelector(`img[src='${thumbSrc}']`);
 
-slider.addEventListener("mouseleave", () => {
-  isDown = false;
-  slider.classList.remove("active");
-});
+    if (matchedSlide) {
+      const thumbOffsetLeft = thumb.offsetLeft;
+      const thumbWidth = thumb.offsetWidth;
+      const thumbsContainerWidth = thumbsContainer.offsetWidth;
+      let scrollLeft =
+        thumbOffsetLeft - (thumbsContainerWidth - thumbWidth) / 2;
 
-slider.addEventListener("mouseup", () => {
-  isDown = false;
-  slider.classList.remove("active");
-});
+      if (scrollLeft < 0) {
+        scrollLeft = 0;
+      } else if (
+        scrollLeft >
+        thumbsContainer.scrollWidth - thumbsContainerWidth
+      ) {
+        scrollLeft = thumbsContainer.scrollWidth - thumbsContainerWidth;
+      }
 
-slider.addEventListener("mousemove", (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 2;
-  slider.scrollLeft = scrollLeft - walk;
-});
+      thumbsContainer.scroll({
+        left: scrollLeft,
+        behavior: "smooth",
+      });
 
-const thumbItems = document.querySelector(".thumbs");
-let thumbDown = false;
-let thumbStartX;
-let thumbScrollLeft;
-
-thumbItems.addEventListener("mousedown", (e) => {
-  thumbDown = true;
-  thumbStartX = e.pageX - thumbItems.offsetLeft;
-  thumbScrollLeft = thumbItems.scrollLeft;
-});
-
-thumbItems.addEventListener("mouseleave", () => {
-  thumbDown = false;
-});
-
-thumbItems.addEventListener("mouseup", () => {
-  thumbDown = false;
-});
-
-thumbItems.addEventListener("mousemove", (e) => {
-  if (!thumbDown) return;
-  e.preventDefault();
-  const thumbX = e.pageX - thumbItems.offsetLeft;
-  const thumbWalk = (thumbX - thumbStartX) * 2;
-  thumbItems.scrollLeft = thumbScrollLeft - thumbWalk;
-});
-
-const thumbItemsArray = Array.from(document.querySelectorAll(".thumb-item"));
-thumbItemsArray.forEach((thumb, index) => {
-  thumb.addEventListener("click", () => {
-    const thumbLeft = thumb.offsetLeft;
-    const thumbWidth = thumb.offsetWidth;
-    const thumbsWidth = thumbItems.offsetWidth;
-    if (thumbLeft + thumbWidth > thumbsWidth) {
-      thumbItems.scrollLeft += thumbLeft + thumbWidth - thumbsWidth;
+      const sliderScrollLeft =
+        matchedSlide.offsetLeft -
+        (slider.offsetWidth - matchedSlide.offsetWidth) / 2;
+      slider.scroll({
+        left: sliderScrollLeft,
+        behavior: "smooth",
+      });
     }
-    slider.scrollLeft = index * slider.offsetWidth;
   });
 });
