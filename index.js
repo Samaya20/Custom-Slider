6b20983eb1e4ebf62892a74ad80fe51d7e2d72c0
Samaya20 <mughumova@gmail.com>
2024-05-15 17:44:38 +0400

    v3

--- index.js
+++ index.js
@@ -1,40 +1,19 @@
-const slider = document.querySelector(".slider");
 const thumbs = document.querySelectorAll(".thumb-item");
 const thumbsContainer = document.querySelector(".thumbs");
+const slider = document.querySelector(".slider");
 
-thumbs.forEach((thumb) => {
-  thumb.addEventListener("click", function () {
-    const thumbSrc = this.getAttribute("src");
-    const matchedSlide = slider.querySelector(`img[src='${thumbSrc}']`);
-
-    if (matchedSlide) {
-      const thumbOffsetLeft = thumb.offsetLeft;
-      const thumbWidth = thumb.offsetWidth;
-      const thumbsContainerWidth = thumbsContainer.offsetWidth;
-      let scrollLeft =
-        thumbOffsetLeft - (thumbsContainerWidth - thumbWidth) / 2;
-
-      if (scrollLeft < 0) {
-        scrollLeft = 0;
-      } else if (
-        scrollLeft >
-        thumbsContainer.scrollWidth - thumbsContainerWidth
-      ) {
-        scrollLeft = thumbsContainer.scrollWidth - thumbsContainerWidth;
-      }
-
-      thumbsContainer.scroll({
-        left: scrollLeft,
-        behavior: "smooth",
-      });
-
-      const sliderScrollLeft =
-        matchedSlide.offsetLeft -
-        (slider.offsetWidth - matchedSlide.offsetWidth) / 2;
-      slider.scroll({
-        left: sliderScrollLeft,
-        behavior: "smooth",
-      });
-    }
-  });
-});
+thumbs.forEach(thumb =>
+    thumb.addEventListener("click", () => {
+        const matchedSlide = slider.querySelector(`img[src='${thumb.getAttribute("src")}']`);
+        if (matchedSlide) {
+            thumbsContainer.scrollTo({
+                left: thumb.offsetLeft - (thumbsContainer.offsetWidth - thumb.offsetWidth) / 2,
+                behavior: "smooth"
+            });
+            slider.scrollTo({
+                left: matchedSlide.offsetLeft - (slider.offsetWidth - matchedSlide.offsetWidth) / 2,
+                behavior: "smooth"
+            });
+        }
+    })
+);
